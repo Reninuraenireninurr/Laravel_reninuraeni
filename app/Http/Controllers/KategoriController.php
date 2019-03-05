3f0d143974f54e2fc0522b1917424d6e419a4ace
Reninuraenireninurr <renin889@gmail.com>
2019-03-05 10:53:19 +0700

    eps21dan22

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Kategori;
+
+class KategoriController extends Controller
+{
+    public function daftar(Request $req)
+    {
+    	$data = Kategori::where('nama_kategori','like',"%{$req->keyword}%")->paginate(10);
+
+    	return view('admin.pages.kategori.daftar',['data'=>$data]);
+    }
+    public function add()
+    {
+    	return view('admin.pages.kategori.add');
+    }
+    public function save(Request $req)
+    {
+    	return 'Fungsi Save';
+    }
+}
