deea80caeafbacfd5b78ac79d345a11b5b491d91
Reninuraenireninurr <renin889@gmail.com>
2019-03-06 14:16:39 +0700

    eps25

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -32,4 +32,13 @@ class KategoriController extends Controller
             return back()->with('result','fail')->withInput();
         }
     }
+    public function edit($id)
+    {
+        $data = Kategori::where('id',$id)->first();
+        return view('admin.pages.kategori.edit',['rc'=>$data]);
+    }
+    public function update(Request $req)
+    {
+       return 'Fungsi Update';
+    }
 }
